ef38ddf4537285bcb77f048d6ee255a2ba9cd2ee
Roc Granada <granadaroc96@gmail.com>
2019-02-20 01:33:53 +0100

    Ongoing

--- house-prices/predice-house.py
+++ house-prices/predice-house.py
@@ -9,13 +9,16 @@ from joblib import dump
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from math import log10, pow
+from math import log10, pow, sqrt
 from random import randint
 
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn import model_selection
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from datasetfunctions import correlation_matrix, show_info, downcast_dtypes
+
+metric = {'neg_mean_squared_error': 'MSE', 'neg_mean_absolute_error': 'MAE', 'r2': 'R^2'}
 
 # Load data
 print("Reading data...")
@@ -37,9 +40,9 @@ correlation_values = [0.79, 0.71, 0.64, 0.62, 0.61, 0.61, 0.56, 0.53, 0.52]
 if 'Id' in train.columns:
     train = train.drop('Id', axis=1)
 train = train.select_dtypes(include=['int64', 'float64'])
-train.SalePrice = train.SalePrice.apply(lambda price: np.log(price))
+train.SalePrice = train.SalePrice.apply(lambda price: np.log10(price))
 
-acc = []
+results = []
 model_list = []
 for _ in range(1,11):
 
@@ -47,6 +50,7 @@ for _ in range(1,11):
     train = train.sample(frac=1).reset_index(drop=True)
     seed1 = randint(0, 100)
     seed2 = randint(0, 100)
+    seed3 = randint(0, 100)
 
     y = train['SalePrice'].values
     X = train.drop('SalePrice', axis=1).values
@@ -54,17 +58,22 @@ for _ in range(1,11):
                                                         test_size=0.2,
                                                         random_state=seed1)
 
-    """
+
     # Regression model
     model = xgb.XGBRegressor(max_depth=3, min_child_weight=0.3,
                              subsample=1, eta=0.3,
                              num_round=10000, seed=seed2)
 
-    model.fit(X_train, y_train, eval_metric='rmse')
+    model.fit(X_train, y_train, eval_metric='mae')
     model_list.append(model)
 
-    score = model.score(X_test, y_test)
-    acc.append(score)
+    kfold = model_selection.KFold(n_splits=10, random_state=seed3)
+    scoring = 'neg_mean_squared_error'
+    #scoring = 'neg_mean_absolute_error'
+    scoring = 'r2'
+    score = model_selection.cross_val_score(model, X_test, y_test,
+                                            cv=kfold, scoring=scoring)
+    results.append(score.mean())
     """
 
     # DMatrix
@@ -72,31 +81,44 @@ for _ in range(1,11):
     param = {'max_depth': 10, 'eta': 0.2, 'silent': 1, 'num_round': 20000}
     param['nthread'] = 4
     param['eval_metric'] = 'mae'
-    bst = xgb.train(param, dtrain)
+    model = xgb.train(param, dtrain)
+    model_list.append(model)
 
-    dtest = xgb.DMatrix(X_test)
-    predict = bst.predict(dtest)
-    print(type(predict), type(y_test))
-    score = accuracy_score(y_test, predict)
-    acc.append(score)
+    dtest = xgb.DMatrix(X_test, y_test)
+    y_predicted = model.predict(dtest)
+    plt.figure(figsize=(10, 5))
+    plt.scatter(y_test, y_predicted, s=20)
+    # score = sqrt(mean_squared_error(y_test, y_predicted))
+    score = mean_absolute_error(y_test, y_predicted)
+    # score = r2_score(y_test, y_predicted)
+    plt.title(''.join(['Predicted vs. Actual.', ' mae = ', str(score)]))
+    plt.xlabel('Actual y')
+    plt.ylabel('Predicted y')
+    plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)])
+    plt.tight_layout()
+    #plt.show()
+    results.append(score)
+    scoring = 'r2'"""
 
-    print("Iteration {0: >2} | Accuracy {1: >4}%".format(
-                                                    _, round(score * 100, 2)))
 
-print("\nMean Accuracy: {}%".format(round(np.mean(acc) * 100, 2)))
-print("Best model: {}".format(np.argmax(acc)+1))
-sys.exit()
-predict = model.predict(test.values)
+    print("Iteration {0: >2} | {1} {2: 2.4f} ({3: 1.3f} std)".format(
+           _, metric[scoring], score.mean(), score.std()))
+
+# print("\nMean MAE: {}".format(round(np.mean(results), 2)))
+# print("Best model: {}".format(np.argmin(results)+1))
 
 """
-dtest = xgb.DMatrix(test_)
-predict = bst.predict(dtest)
-xgb.plot_importance(bst)
-xgb.plot_tree(bst, num_trees=2)
-plt.show()
+best_model = model_list[np.argmin(results)]
+submission_test = submission[train.drop('SalePrice', axis=1).columns]
+dtest = xgb.DMatrix(submission_test.values)
+predict = best_model.predict(dtest)
 """
-test['SalePrice'] = pd.Series(predict)
-results = test[['Id', 'SalePrice']]
+
+best_model = model_list[np.argmin(results)]
+predict = best_model.predict(submission[train.drop('SalePrice', axis=1).columns].values)
+
+submission['SalePrice'] = pd.Series(predict)
+results = submission[['Id', 'SalePrice']]
 results['SalePrice'] = results['SalePrice'].apply(lambda price: pow(10, price))
 results.to_csv('submission.csv', index=False)
 
